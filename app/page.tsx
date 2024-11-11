6ef2f210100fc4e545f26c8e93a349f8fc333529
Pezcadena <angellopezcadena@gmail.com>
2024-11-10 19:53:05 -0600

    feat(Page): add upload button

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,28 @@
 'use client'
 
-import { useState } from "react";
-import Scene from "./scene"
+import { useState } from 'react'
+import Scene from './scene'
 
 export default function Page() {
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null)
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const file = event.target.files?.[0]
     if (file) {
-      const url = URL.createObjectURL(file);
-      setImageUrl(url);
+      const url = URL.createObjectURL(file)
+      setImageUrl(url)
     }
-  };
+  }
   return (
     <>
-      <input type="file" accept="image/*" onChange={handleImageUpload} />
-      <div className="h-[600px]">
-        { imageUrl && <Scene imageUrl={imageUrl}/>}
-      </div>
+      <main>
+        <div className='h-[80vh]'>{imageUrl && <Scene imageUrl={imageUrl} />}</div>
+        <div className='flex w-full justify-center'>
+          <fieldset className='rounded-xl rounded-ss-none bg-[#83322D] p-4 text-center font-bold text-white'>
+            <label htmlFor='file-upload'>Seleccionar Imagen</label>
+            <input id='file-upload' type='file' accept='image/' onChange={handleImageUpload} className='hidden' />
+          </fieldset>
+        </div>
+      </main>
     </>
   )
 }
