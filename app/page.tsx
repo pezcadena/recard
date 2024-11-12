40d815441c975125c20a3b683749c616b3e9b914
Pezcadena <angellopezcadena@gmail.com>
2024-11-11 19:50:07 -0600

    feat(Page): add title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 'use client'
-
+import { Poppins } from '@next/font/google'
 import { useState } from 'react'
 import Scene from './scene'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['200', '400', '700', '900'], // Añade los pesos que necesites
+})
 
 export default function Page() {
   const [imageUrl, setImageUrl] = useState<string | null>(null)
@@ -14,11 +19,25 @@ export default function Page() {
   }
   return (
     <>
-      <main>
-        <div className='h-[80vh]'>{imageUrl && <Scene imageUrl={imageUrl} />}</div>
-        <div className='flex w-full justify-center'>
-          <fieldset className='rounded-xl rounded-ss-none bg-[#83322D] p-4 text-center font-bold text-white'>
-            <label htmlFor='file-upload'>Seleccionar Imagen</label>
+      <main className={poppins.className}>
+        <header className='flex w-full justify-center border-t-[15px] border-[#416aa3] p-3 pb-0'>
+          <h1 className='text-4xl font-extrabold text-[#416aa3]'>
+            <span className='text-[#83322D]'>Re</span>Card
+          </h1>
+        </header>
+        {imageUrl && (
+          <div className='h-[80vh]'>
+            <Scene imageUrl={imageUrl} />
+          </div>
+        )}
+        <div className='flex w-full justify-center p-5'>
+          <fieldset className='relative flex'>
+            <label
+              className='cursor-pointer rounded-xl rounded-ss-none bg-[#83322D] p-4 text-center font-bold text-white hover:bg-[#C0605B]'
+              htmlFor='file-upload'
+            >
+              Seleccionar Imagen
+            </label>
             <input id='file-upload' type='file' accept='image/' onChange={handleImageUpload} className='hidden' />
           </fieldset>
         </div>
